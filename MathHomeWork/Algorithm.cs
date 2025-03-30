d18211d2f199c238dbbd8ffb39decc4b3b208879
wrwsc <destr0yka23@gmail.com>
2025-03-30 21:35:33 +0500

    math solution

---
+++ MathHomeWork/Algorithm.cs
@@ -0,0 +1,73 @@
+﻿namespace MathHomeWork;
+
+internal static class Algorithm
+{
+    internal static void Print(int a, int b, int m)
+    {
+        // НОД
+        var gcd = Gcd(a, m, out var x1, out var y1);
+        Console.WriteLine($"НОД({a}, {m}) = {gcd}");
+
+        // Делимость b на НОД
+        if (b % gcd != 0)
+        {
+            Console.WriteLine($"b не делится на НОД.");
+            return;
+        }
+
+        // Сокращаем уравнение
+        var first = a / gcd;
+        var second = b / gcd;
+        var mod = m / gcd;
+        Console.WriteLine($"Сокращенное уравнение: {first}x = {second} (mod {mod})");
+
+        // Обратный элемент
+        var r = (Reverse(first, mod) * second) % mod;
+        if (r < 0) r += mod;
+        Console.WriteLine($"P(n-1): {r} (mod {mod})");
+
+        // Общее решение
+        DisplayAllSolutions(gcd, r, mod);
+    }
+
+    private static int Gcd(int a, int b, out int x, out int y)
+    {
+        if (b == 0)
+        {
+            x = 1; 
+            y = 0;
+            return a;
+        }
+        var gcd = Gcd(b, a % b, out var x1, out var y1);
+        x = y1;
+        y = x1 - (a / b) * y1;
+        return gcd;
+    }
+    
+    private static int Reverse(int a, int m)
+    {
+        var gcd = Gcd(a, m, out var x, out _);
+        if (gcd != 1) Console.WriteLine("Обратного элемента нет");
+        return (x % m + m) % m;
+    }
+    
+    private static void DisplayAllSolutions(int gcd, int x0, int m1)
+    {
+        Console.WriteLine("Все решения: ");
+        var temp = 0;
+        Console.WriteLine("  ~");
+        for (var k = 0; k < gcd; k++)
+        {
+            temp++;
+            var xk = x0 + k * m1;
+            var middle = gcd / 2;
+            if (k == middle)
+            {
+                Console.WriteLine($"< x{k} = {xk}");
+            }
+            Console.WriteLine($" |x{k} = {xk}");
+        }
+        Console.WriteLine("  ~");
+        Console.WriteLine($"Кол-во решений: {temp}");
+    }
+}
---
+++ MathHomeWork/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MathHomeWork;
+
+class Program
+{
+    private static int a = 14546;
+    private static int m = 19929;
+    private static int alpha = 4;
+    private static int b = 7 * alpha;
+    
+    public static void Main(string[] args)
+    {
+        Algorithm.Print(a, b, m);
+    }
+}
